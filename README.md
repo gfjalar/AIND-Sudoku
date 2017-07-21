06f324881227fc200f597634a293906afaa501d3
Piotr Galar <fjalar.galar@gmail.com>
2017-07-21 15:36:45 +0200

    submission #1

--- README.md
+++ README.md
@@ -3,17 +3,17 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+A: If there are such two boxes in a unit which allow only a certain pair of digits, then none of the other boxes can be assigned any of these digits. The constraint can be enforced by iterating over all the units, finding such pairs in them and removing appropriate digits from the outstanding boxes. The `Naked Twins` technique can be combined with `Elimination` and `Only Choice` techniques in `Reduce Puzzle` step of solving a sudoku. The technique can be extanded to account for triplets, quadruplets, etc. [*implemented as `naked_multiples`*]
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
+A: All the boxes belonging to each of the diagonals of a sudoku are restricted to have unique values. The constraint can be enforced by implementing the diagonals as two additional units and storing them together with the classical row, column and square units. Expanding the units definition broadens the search space for `Elimination`, `Only Choice` and `Naked Twins` techniques.
 
 ### Install
 
 This project requires **Python 3**.
 
-We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project. 
+We recommend students install [Anaconda](https://www.continuum.io/downloads), a pre-packaged Python distribution that contains all of the necessary libraries and software for this project.
 Please try using the environment we provided in the Anaconda lesson of the Nanodegree.
 
 ##### Optional: Pygame
@@ -41,4 +41,3 @@ The setup is simple.  If you have not installed the client tool already, then yo
 To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
 
 This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
-
--- solution.py
+++ solution.py
@@ -15,6 +15,24 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
+def naked_multiples(values, multiples):
+    """Eliminate values using the naked multiples strategy.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns:
+        the values dictionary with the naked multiples eliminated from peers.
+    """
+    for unit in unit_list:
+        multiple_digits = [values[box] for box in unit if len(values[box]) == multiples]
+        multiple_digits = set([digit for digit in multiple_digits if multiple_digits.count(digit) == multiples])
+        for multiple_digit in multiple_digits:
+            for box in unit:
+                if values[box] != multiple_digit:
+                    for digit in multiple_digit:
+                        values = assign_value(values, box, values[box].replace(digit, ''))
+    return values
+
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
     Args:
@@ -23,13 +41,47 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
+    return naked_multiples(values, 2)
 
-    # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+def naked_triplets(values):
+    """Eliminate values using the naked triplets strategy.
+    Args:
+        values(dict): a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns:
+        the values dictionary with the naked triplets eliminated from peers.
+    """
+    return naked_multiples(values, 3)
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [a + b for a in A for b in B]
+
+def merge(A, B):
+    "Join elements of zipping elements of A with elements of B."
+    return [a + b for (a, b) in zip(A, B)]
+
+def chunk(A, n):
+    "Split of elements in A into chunks of size n"
+    return [A[i : i + n] for i in range(0, len(A), n)]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+boxes = cross(rows, cols)
+
+row_triples = chunk(rows, 3)
+col_triples = chunk(cols, 3)
+
+row_units = [cross(row, cols) for row in rows]
+col_units = [cross(rows, col) for col in cols]
+square_units = [cross(row_triple, col_triple) for row_triple in row_triples for col_triple in col_triples]
+diag_units = [merge(rows, cols), merge(rows, cols[::-1])]
+
+unit_list = row_units + col_units + square_units + diag_units
+
+units = dict((box, [unit for unit in unit_list if box in unit]) for box in boxes)
+peers = dict((box, set(sum(units[box], [])) - set([box])) for box in boxes)
 
 def grid_values(grid):
     """
@@ -41,7 +93,8 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    return dict([(box, '123456789' if char == '.' else char)
+            for (box, char) in zip(boxes, grid)])
 
 def display(values):
     """
@@ -49,19 +102,97 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    if values is False:
+        print('We could not display your board. Solution was not found.')
+        return
+    width = 1 + max(len(values[box]) for box in boxes)
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for row in rows:
+        print(''.join(values[row + col].center(width) +
+                ('|' if col in '36' else '') for col in cols))
+        if row in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    for box in solved_boxes(values):
+        for peer in peers[box]:
+            values = assign_value(values, peer, values[peer].replace(values[box], ''))
+    return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unit_list:
+        for digit in '123456789':
+            box = [box for box in unit if digit in values[box]]
+            if len(box) == 1:
+                values = assign_value(values, box[0], digit)
+    return values
+
+def invalid_boxes(values):
+    "Boxes with invalid digit values(size 0)."
+    return [box for (box, digit) in values.items() if len(digit) == 0]
+
+def solved_boxes(values):
+    "Boxes which are already solved(size 1)"
+    return [box for (box, digit) in values.items() if len(digit) == 1]
+
+def unsolved_boxes(values):
+    "Boxes which are not yet solved(size greater than 1)."
+    return [box for (box, digit) in values.items() if len(digit) > 1]
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate(), only_choice(), naked_twins() and naked_triplets(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    unsolved_before = unsolved_boxes(values)
+    values = eliminate(values)
+    values = only_choice(values)
+    values = naked_twins(values)
+    values = naked_triplets(values)
+    unsolved_after = unsolved_boxes(values)
+    if len(invalid_boxes(values)) != 0:
+        return False
+    if len(unsolved_before) == len(unsolved_after):
+        return values
+    return reduce_puzzle(values)
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, try all possible values."
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    unsolved = unsolved_boxes(values)
+    if len(unsolved) == 0:
+        return values
+    unsolved = sorted(unsolved, key = lambda box: len(values[box]))
+    box = unsolved[0]
+    for digit in values[box]:
+        guessed_values = assign_value(values.copy(), box, digit)
+        found_values = search(guessed_values)
+        if found_values is not False:
+            return found_values
+    return False
 
 def solve(grid):
     """
@@ -72,6 +203,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    return search(grid_values(grid))
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
